7004d5d7bbc4ce6f6402b39d041efe6fc6b4daab
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2024-04-22 12:54:50 +0200

    add lnn

---
+++ lnn/README.md
@@ -0,0 +1,6 @@
+### lnn
+Just some neural network shenanigans.
+
+#### Todo
+- Get working `basic_nn.py`
+- Write lib in C
--- lnn/basic_nn.py
+++ lnn/basic_nn.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from tqdm import trange
 
 # comment the entire code, explain everything, and generate graphs
@@ -17,91 +16,87 @@ def sigmoid_deriv(x):
 
 def forward(x, w1, w2):
 	h1 = x.dot(w1)
-		a1 = sigmoid(h1)
+	a1 = sigmoid(h1)
 
-		h2 = a1.dot(w2)
-		a2 = sigmoid(h2)
+	h2 = a1.dot(w2)
+	a2 = sigmoid(h2)
 
-		return a2
+	return a2
 
 def generate_wt(x, y):
 	l = []
-		for _ in range(x * y):
-			l.append(np.random.randn())
-		return np.array(l).reshape(x, y)
+	for _ in range(x * y):
+		l.append(np.random.randn())
+	return np.array(l).reshape(x, y)
 
 def loss(out, y):
 	s = np.square(out - y)
-		s = np.sum(s) / len(y)
+	s = np.sum(s) / len(y)
 
-		return s
+	return s
 
 def back_prop(x, y, w1, w2, lr):
+	# forward
 	h1 = x.dot(w1)
-		a1 = sigmoid(h1)
+	a1 = sigmoid(h1)
 
-		h2 = h1.dot(w2)
-		a2 = sigmoid(h2) # output (4,1)
+	h2 = h1.dot(w2)
+	a2 = sigmoid(h2)
 
-		#L = (a2 - y) # loss
+	# backward
+	d2 = (a2 - y) # error, a2 is output of nn, y is real output
+	d1 = np.dot(w2.dot(d2), sigmoid_deriv(a1))
 
-		# gradients
-		#dL = 1
+	# grads
+	w1_grad = x.dot(d1)
+	w2_grad = a1.dot(d2[0])
+	print(w1_grad)
+	print(w2_grad)
 
-		#da2_da1 = w2
-		#dL_dw2 = a1
+	# update grads
+	w1 = w1 - (lr * w1_grad)
+	w2 = w2 - (lr * w2_grad)
 
-		#da1_dx = w1
-		#da1_dw1 = x
-
-		d2 = (a2 - y) # loss
-		d1 = np.dot(w2.dot(d2.transpose()), sigmoid_deriv(a1))
-
-		# gradients
-		w1_grad = x.transpose().dot(d1.transpose())
-		w2_grad = a1.transpose().dot(d2)
-
-		w1 = w1 - (lr * w1_grad)
-		w2 = w2 - (lr * w2_grad)
-
-		return w1, w2
+	return w1, w2
 
 def train(x, y, w1, w2, lr=0.01, epochs=1):
 	losses = []
 
-		for _ in (t := trange(1, epochs+1)):
-			l = []
-				out = forward(x, w1, w2)
-				l.append(loss(out, y))
+	for _ in (t := trange(1, epochs+1)):
+		l = [] # loss values for each training example
 
-				w1, w2 = back_prop(x, y, w1, w2, lr)
+		for i in range(len(x)):
+			out = forward(x[i], w1, w2)
+			l.append(loss(out, y[i]))
+			w1, w2 = back_prop(x[i], y[i], w1, w2, lr)
 
-				losses.append(sum(l) / len(x))
-				t.set_description("loss: %.3f" % (losses[-1]))
-
-		return losses, w1, w2
+		losses.append(sum(l) / len(x))
+		t.set_description("loss: %.3f" % (losses[-1]))
+	return losses, w1, w2
 
 def main():
 	x = [[0.0, 0.0],
-			[0.0, 1.0],
-			[1.0, 0.0],
-			[1.0, 1.0]]
+		[0.0, 1.0],
+		[1.0, 0.0],
+		[1.0, 1.0]]
 
 	y = [[0.0],
-			[1.0],
-			[1.0],
-			[0.0]]
+		[1.0],
+		[1.0],
+		[0.0]]
 
 	x = np.array(x, dtype=np.float32)
-		y = np.array(y, dtype=np.float32)
+	y = np.array(y, dtype=np.float32)
 
-		w1 = generate_wt(2, 4)
-		w2 = generate_wt(4, 1)
+	w1 = generate_wt(2, 2)
+	w2 = generate_wt(5, 1)
 
-		_, w1, w2 = train(x, y, w1, w2, 0.01, 1000)
+	_, w1, w2 = train(x, y, w1, w2, 0.01, 1000)
 
-		print("prediction:", forward(x, w1, w2).transpose())
-		print("prediction:", y.transpose())
+	print("prediction")
+	for i in range(len(x)):
+		print(forward(x[i], w1, w2))
+	print("actual:", y.transpose())
 
 if __name__ == "__main__":
 	main()
