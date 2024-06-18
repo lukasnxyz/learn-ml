9703d88e177b469104fbbbe6d189b088d282c13c
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2024-06-18 11:13:21 +0200

    edit msc/regression_line.py

--- msc/regression_line.py
+++ msc/regression_line.py
@@ -2,15 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import sqrt
 
-def least_squares_reg(x: np.ndarray, y: np.ndarray) -> list:
-    N = len(x) if len(x) == len(y) else 0
-    m = (N*sum(x*y) - sum(x)*sum(x)) / (N*sum(x**2) - sum(x)**2)
-    b = (sum(y) - m*sum(x)) / N
-    return m, b
-
-def l_of_b_fit(x: np.ndarray, y: np.ndarray) -> list:
-    m = (y[-1]-y[0])/(x[-1]-x[0])
-    b = y[0]-m*x[0]
+def lsr(x: np.ndarray, y: np.ndarray) -> list:
+    m = (sum(x*y)-sum(x)*sum(x))/(sum(x**2)-sum(x)**2)
+    b = (sum(y)-m*sum(x))/len(x)
     return m, b
 
 def corr_coef(x: np.ndarray, y: np.ndarray) -> float: 
@@ -29,7 +23,8 @@ if __name__ == "__main__":
     y = np.array([45, 47, 39, 58, 68, 76, 75, 74, 78, 81])
     print(f"x: min:{min(x)}, max:{max(x)}\ny: min:{min(y)}, max:{max(y)}")
 
-    m, b = l_of_b_fit(x, y)
+    #m, b = l_of_b_fit(x, y)
+    m, b = lsr(x, y)
     print(f"y = {m:.3f}x + {b:.3f}")
     r = corr_coef(x, y)
     f = lambda x : m*x + b
